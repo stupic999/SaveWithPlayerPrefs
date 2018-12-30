cb1839288da9a7ade7b4ad0dae3997f98dd3fdea
stupid999 <aierzhiguang2@gmail.com>
2018-12-30 23:35:42 +0800

    学习记录： 能用playerPrefs储存资料

---
+++ Assets/PlayerWeopen.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerWeopen : MonoBehaviour {
+
+    public Text text;
+    int weopenNum;
+
+    // Use this for initialization
+    void Awake()
+    {
+      //  PlayerPrefs.DeleteAll();
+
+        if (!PlayerPrefs.HasKey("WeopenNumber"))
+        {
+            PlayerPrefs.SetInt("WeopenNumber", 0);
+            text.text = "0";
+            PlayerPrefs.Save();
+        }
+        else
+        {
+            text.text = PlayerPrefs.GetInt("WeopenNumber").ToString();
+            weopenNum = PlayerPrefs.GetInt("WeopenNumber");
+        }
+	}
+
+    public void Add()
+    {
+        weopenNum++;
+        text.text = weopenNum.ToString();
+    }
+
+    public void Save()
+    {
+        PlayerPrefs.SetInt("WeopenNumber", weopenNum);
+    }
+}
